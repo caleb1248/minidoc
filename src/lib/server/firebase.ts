062c6bb29f964f05dfda6ef80b41a90a438e22d0
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-11-05 07:42:28 -0500

    screw server side stuff

--- src/lib/server/firebase.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase-admin/app';
-import { SERVICE_ACCT } from '$env/static/private';
-
-const app = initializeApp({
-	credential: JSON.parse(SERVICE_ACCT)
-});
-
-export { app };
